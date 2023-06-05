a8da1f1c5e5367aacfd9cfedfd683b6d859e3c31
ORiverB <hago8556@gmail.com>
2023-06-05 15:02:18 +0300

    сделал...?

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import redlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
 const sayGreetings = () => {
   console.log('Welcome to the Brain Games!');
-  const name = redlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
