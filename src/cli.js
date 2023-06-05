ac9e5089af233a39e1641ad78c278a6231e2d151
ORiverB <hago8556@gmail.com>
2023-06-05 14:53:06 +0300

    норм?

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const sayGreetings = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`hello, ${name}!`);
+  console.log(`Hello, ${name}!`);
 };
 
 export default sayGreetings;
